940c76104826a4bd6c868abb71a7edd31fa2e19c
mahanfr <mahan.farzaneh@gmail.com>
2025-12-10 20:36:39 +0330

    Loading models and skybox

--- src/main.c
+++ src/main.c
@@ -1,61 +1,74 @@
 #include "../raylib/include/raylib.h"
-
-typedef enum {
-    SPRITE_DIR_UP = 0,
-    SPRITE_DIR_DOWN,
-    SPRITE_DIR_RIGHT,
-    SPRITE_DIR_LEFT,
-} SpriteDir;
-
-typedef struct {
-    Transform transform;
-    Texture2D tIdle[4];
-    Rectangle spriteFrame;
-} Player;
-
-void loadPlayerIdleTexture(Player *player) {
-    const char* locUp    = "./assets/player/idle/idle_up.png";
-    const char* locDown  = "./assets/player/idle/idle_down.png";
-    const char* locRight = "./assets/player/idle/idle_right.png";
-    const char* locLeft  = "./assets/player/idle/idle_left.png";
-    player->tIdle[SPRITE_DIR_UP]    = LoadTexture(locUp);
-    player->tIdle[SPRITE_DIR_DOWN]  = LoadTexture(locDown);
-    player->tIdle[SPRITE_DIR_RIGHT] = LoadTexture(locRight);
-    player->tIdle[SPRITE_DIR_LEFT]  = LoadTexture(locLeft);
-}
-
-void unloadPlayerIdleTexture(Player *player) {
-    UnloadTexture(player->tIdle[SPRITE_DIR_UP]);
-    UnloadTexture(player->tIdle[SPRITE_DIR_DOWN]);
-    UnloadTexture(player->tIdle[SPRITE_DIR_RIGHT]);
-    UnloadTexture(player->tIdle[SPRITE_DIR_LEFT]);
-}
+#include "../raylib/include/rlgl.h"
 
 int main(void) {
     InitWindow(800, 600, "The Monument");
 
-    Player player = {0};
-    player.transform.translation.x = GetScreenWidth() / 2.0f - 16;
-    player.transform.translation.y = GetScreenHeight() / 2.0f - 16;
-    loadPlayerIdleTexture(&player);
+    Model model = LoadModel("./assets/SpaceShipV1.obj");
+    Camera3D camera = {0};
+    camera.position = (Vector3){ 5.0f, 4.0f, 5.0f };    // Camera position
+    camera.target = (Vector3){ 0.0f, 2.0f, 0.0f };      // Camera looking at point
+    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
+    camera.fovy = 45.0f;                                // Camera field-of-view Y
+    camera.projection = CAMERA_PERSPECTIVE;             // Camera projection type
 
+    Mesh cube = GenMeshCube(1.0f, 1.0f, 1.0f);
+    Model skybox = LoadModelFromMesh(cube);
+
+    skybox.materials[0].shader = LoadShader(
+            "./assets/shaders/glsl330/skybox.vs",
+            "./assets/shaders/glsl330/skybox.fs");
+    SetShaderValue(
+            skybox.materials[0].shader,
+            GetShaderLocation(skybox.materials[0].shader, "environmentMap"),
+            (int[1]) {MATERIAL_MAP_CUBEMAP},
+            SHADER_UNIFORM_INT);
+    SetShaderValue(
+            skybox.materials[0].shader,
+            GetShaderLocation(skybox.materials[0].shader, "doGamma"),
+            (int[1]) { 0 },
+            SHADER_UNIFORM_INT);
+    SetShaderValue(
+            skybox.materials[0].shader,
+            GetShaderLocation(skybox.materials[0].shader, "vflipped"),
+            (int[1]) { 0 },
+            SHADER_UNIFORM_INT);
+
+    Shader shdrCubemap = LoadShader(
+            "./assets/shaders/glsl330/cubemap.vs",
+            "./assets/shaders/glsl330/cubemap.fs");
+    SetShaderValue(
+            shdrCubemap,
+            GetShaderLocation(shdrCubemap, "equirectangularMap"),
+            (int[1]){ 0 },
+            SHADER_UNIFORM_INT);
+
+    Image sb_image = LoadImage("./assets/skybox/corona_up.png");
+    skybox.materials[0].maps[MATERIAL_MAP_CUBEMAP].texture = LoadTextureCubemap(sb_image, CUBEMAP_LAYOUT_AUTO_DETECT);
+    UnloadImage(sb_image);
+
+    SetTargetFPS(60);
     while (!WindowShouldClose()) {
-        float deltaTime = GetFrameTime() * 60;
-        if (IsKeyDown(KEY_UP)) {
-            player.transform.translation.y -= 2 * deltaTime;
-        } else if (IsKeyDown(KEY_DOWN)) {
-            player.transform.translation.y += 2 * deltaTime;
-        } else if (IsKeyDown(KEY_RIGHT)) {
-            player.transform.translation.x += 2 * deltaTime;
-        } else if (IsKeyDown(KEY_LEFT)) {
-            player.transform.translation.x -= 2 * deltaTime;
-        }
+        UpdateCamera(&camera, CAMERA_FIRST_PERSON);
         BeginDrawing();
-            ClearBackground(RAYWHITE);
-            DrawRectangle(player.transform.translation.x, player.transform.translation.y, 32, 32, RED);
+            ClearBackground(GetColor(0x181818FF));
+            BeginMode3D(camera);
+                rlDisableBackfaceCulling();
+                rlDisableDepthMask();
+                    DrawModel(skybox, (Vector3) {0,0,0}, 1.0f, WHITE);
+                rlEnableBackfaceCulling();
+                rlEnableDepthMask();
+
+                DrawModel(model, (Vector3) {0}, 1.0f, WHITE);
+                DrawGrid(10, 1.0f);        // Draw a grid
+            EndMode3D();
         EndDrawing();
     }
-    unloadPlayerIdleTexture(&player);
+    UnloadShader(skybox.materials[0].shader);
+    UnloadTexture(skybox.materials[0].maps[MATERIAL_MAP_CUBEMAP].texture);
+
+    UnloadModel(skybox);
+    UnloadModel(model);
     CloseWindow();
     return 0;
 }
